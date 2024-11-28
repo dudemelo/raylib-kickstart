58c44f37e66cbd00fefa6d11d45a44a6884e361a
dudemelo <1172271+dudemelo@users.noreply.github.com>
2024-11-28 17:54:04 +0100

    Add initial Raylib script to open a window

--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 
 int main() {
   InitWindow(800, 600, "Raylib Kickstart");
+  SetTargetFPS(60);
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("Raylib Kickstart", 320, 300, 20, WHITE);
+    EndDrawing();
+  }
+  CloseWindow();
   return 0;
 }
